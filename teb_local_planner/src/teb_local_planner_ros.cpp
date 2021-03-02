1ef29db134572fc19b8493a8be398e5759cedf12
mkolodziejczyk-piap <73108914+mkolodziejczyk-piap@users.noreply.github.com>
2021-03-02 15:35:14 +0100

    fix tf

--- teb_local_planner/src/teb_local_planner_ros.cpp
+++ teb_local_planner/src/teb_local_planner_ros.cpp
@@ -715,7 +715,9 @@ bool TebLocalPlannerROS::transformGlobalPlan(const std::vector<geometry_msgs::ms
 //    plan_pose.header.frame_id, plan_to_global_transform);
 
     //let's get the pose of the robot in the frame of the plan
-    geometry_msgs::msg::PoseStamped robot_pose = tf_->transform(global_pose, plan_pose.header.frame_id);
+//     geometry_msgs::msg::PoseStamped robot_pose = tf_->transform(global_pose, plan_pose.header.frame_id);
+geometry_msgs::msg::PoseStamped robot_pose;
+nav_2d_utils::transformPose(tf_, plan_pose.header.frame_id, global_pose, robot_pose, transform_tolerance_))
 
     //we'll discard points on the plan that are outside the local costmap
     double dist_threshold = std::max(costmap.getSizeInCellsX() * costmap.getResolution() / 2.0,
